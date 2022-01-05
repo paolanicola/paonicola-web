5703860c2b633b1e6079498bcfa5f5be8d3a8690
krisstian45 <cristiangma45@gmail.com>
2022-01-04 22:01:38 -0300

    new navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import { NavBar, Logo } from '..';
-import { ReactComponent as ShoppinBag } from '../../assets/images/header/shopping-bag.svg';
+import { NavBar, Logo, PrimaryButton } from '..';
+import { ReactComponent as ShoppingBag } from '../../assets/images/header/shopping-bag.svg';
+import { ReactComponent as InstagramBrand } from '../../assets/images/header/instagram-brands.svg';
+import { ReactComponent as MailBrand } from '../../assets/images/header/mail.svg';
+import { ReactComponent as WhatsappBrand } from '../../assets/images/header/whatsapp-brands.svg';
+import { ReactComponent as NavOpen } from '../../assets/images/header/nav-open.svg';
+import { ReactComponent as NavClose } from '../../assets/images/header/nav-close.svg';
+import { ReactComponent as PaolaLogo } from '../../assets/images/header/logo.svg';
+import LogoWebp from '../../assets/images/header/paola_logo.webp';
 
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 export default function Header() {
   const [size, setSize] = useState(false);
+
   const changeNavbarSize = () => {
     if (window.scrollY >= 60) {
       setSize(true);
@@ -13,21 +21,73 @@ export default function Header() {
       setSize(false);
     }
   };
-  window.addEventListener('scroll', changeNavbarSize);
+  useEffect(() => {
+    window.addEventListener('scroll', changeNavbarSize);
+    return () => {
+      window.removeEventListener('scroll', changeNavbarSize);
+    };
+  });
 
+  const [sidebar, setSidebar] = useState(false);
+
+  const showSidebar = () => setSidebar(!sidebar);
+
+  const closeSidebar = () => setSidebar(!sidebar);
   return (
-    <div className='header'>
-      <div classNAme='header__left'>
-        <NavBar />
+    <>
+      <div className={size ? 'navBar header--resize' : 'navBar'}>
+        <div className='navBar__icon__svg navBar__icon__svg--none fixed' onClick={showSidebar}>
+          <NavOpen className='icon__svg__open' />
+        </div>
+        <div className='menu__logo'>
+          <NavLink to='/'>
+            <img src={LogoWebp} className='menu__logo__svg' alt='logo' />
+          </NavLink>
+        </div>
+
+        <div className={sidebar ? 'navBar__menu navBar__menu--active' : 'navBar__menu'}>
+          <div className='menu__left'>
+            <div className='pepe ' onClick={closeSidebar}>
+              <div className='container__icon__close'></div>
+            </div>
+            <NavLink className='menu__item menu__link' to='/about' onClick={closeSidebar}>
+              <p>Sobre Mi</p>
+            </NavLink>
+            <NavLink className='menu__item menu__link' to='/changes' onClick={closeSidebar}>
+              <p>Cambios Reales</p>
+            </NavLink>
+            <NavLink className='menu__item menu__link' to='/faq' onClick={closeSidebar}>
+              <p>FAQ</p>
+            </NavLink>
+            <NavLink className='menu__item menu__link' to='/contact' onClick={closeSidebar}>
+              <p>Contacto</p>
+            </NavLink>
+          </div>
+
+          <div className='menu__right'>
+            <div className='menu__item menu__link'>
+              <div className='menu__redes '>
+                <Link to='#' className='menu__redes__link'>
+                  <InstagramBrand className='menu__redes__icono' />
+                </Link>
+                <Link to='#' className='menu__redes__link'>
+                  <MailBrand className='menu__redes__icono' />
+                </Link>
+                <Link to='#' className='menu__redes__link'>
+                  <WhatsappBrand className='menu__redes__icono' />
+                </Link>
+              </div>
+            </div>
+            <div className='menu__item menu__link'>
+              <PrimaryButton actionText='Tienda Online' href='/' />
+            </div>
+          </div>
+        </div>
+
+        <div className='navBar__icon__svg'>
+          <ShoppingBag className='' />
+        </div>
       </div>
-      <div className='header__logo'>
-        <Link to='/' className='header__logo'>
-          <Logo className='header__logo__img' />
-        </Link>
-      </div>
-      <div className='header__right'>
-        <ShoppinBag className='header__icon' />
-      </div>
-    </div>
+    </>
   );
 }
--- src/components/PrimaryButton/PrimaryButton.js
+++ src/components/PrimaryButton/PrimaryButton.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function PrimaryButton({ actionText, onClick }) {
+export default function PrimaryButton({ actionText, href, onClick }) {
   return (
-    <button className="primary-button" onClick={onClick}>
-      {actionText}
-    </button>
+    <Link to={href}>
+      <button className='primary-button' onClick={onClick}>
+        {actionText}
+      </button>
+    </Link>
   );
 }
