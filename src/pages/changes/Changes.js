df92c947f181fa2a959f7f5289c86f4af8a88165
krisstian45 <cristiangma45@gmail.com>
2022-04-12 19:06:44 -0300

    add carousel on changes page

--- src/pages/changes/Changes.js
+++ src/pages/changes/Changes.js
@@ -1,7 +1,26 @@
+import React from 'react';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import img1 from '../../assets/images/transformaciones/t-caso1.jpg';
+import img2 from '../../assets/images/transformaciones/t-caso2.jpg';
+import img3 from '../../assets/images/transformaciones/t-caso3.jpg';
+
 export default function Changes() {
   return (
     <>
-      <p>Cambios Reales </p>
+      <div className='container-changes'>
+        <Carousel>
+          <div>
+            <img src={img1} />
+          </div>
+          <div>
+            <img src={img2} />
+          </div>
+          <div>
+            <img src={img3} />
+          </div>
+        </Carousel>
+      </div>
     </>
   );
 }
