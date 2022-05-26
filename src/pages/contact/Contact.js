98b6895a7edb94dad046d46e111dd915a6ca843d
krisstian45 <cristiangma45@gmail.com>
2022-05-26 18:47:25 -0300

    add state to contact

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { ReactComponent as InstagramBrand } from '../../assets/images/header/instagram-brands.svg';
 import { ReactComponent as MailBrand } from '../../assets/images/header/mail.svg';
 import { ReactComponent as WhatsappBrand } from '../../assets/images/header/whatsapp-brands.svg';
 
+import { ToastContainer, toast } from 'react-toastify';
+
 export default function Contact() {
+  const [send, setSend] = useState(false);
   const {
     register,
     handleSubmit,
     formState: { errors },
     clearErrors,
   } = useForm();
-  const onSubmit = (data) => {};
+  const onSubmit = (data) => {
+    toast('Mensaje Enviado Correctamente!');
+    setSend(true);
+  };
 
   return (
     <>
@@ -46,66 +52,70 @@ export default function Contact() {
             </ul>
           </div>
         </div>
-        <div className='contact-right one animate fadeRight'>
-          <form id='formContact' className='form' onSubmit={handleSubmit(onSubmit)}>
-            <div className='form'>
-              <div className='form-name'>
-                <p className='contact-right-label'>
-                  <label>Nombre</label>
-                </p>
-                <input
-                  className={errors.nombre && 'input_error'}
-                  type='text'
-                  {...register('nombre', {
-                    required: {
-                      value: true,
-                      message: 'Nombre requerdio',
-                    },
-                    pattern: {
-                      value: /^[a-z ,.'-]+$/i,
-                      message: 'Formato incorrecto, solo letras',
-                    },
-                  })}
-                />
-                {errors.nombre && <span className={errors.nombre && 'span_error'}>{errors.nombre.message}</span>}
+        {send ? (
+          <div className='contact-right contact-title'>Mensaje enviado Gracias por Contactarnos. </div>
+        ) : (
+          <div className='contact-right one animate fadeRight'>
+            <form id='formContact' className='form' onSubmit={handleSubmit(onSubmit)}>
+              <div className='form'>
+                <div className='form-name'>
+                  <p className='contact-right-label'>
+                    <label>Nombre</label>
+                  </p>
+                  <input
+                    className={errors.nombre && 'input_error'}
+                    type='text'
+                    {...register('nombre', {
+                      required: {
+                        value: true,
+                        message: 'Nombre requerdio',
+                      },
+                      pattern: {
+                        value: /^[a-z ,.'-]+$/i,
+                        message: 'Formato incorrecto, solo letras',
+                      },
+                    })}
+                  />
+                  {errors.nombre && <span className={errors.nombre && 'span_error'}>{errors.nombre.message}</span>}
+                </div>
               </div>
-            </div>
-            <p className='contact-right-label'>
-              <label>E-mail</label>
-            </p>
-            <input
-              className={errors.email && 'input_error'}
-              placeholder=''
-              type='text'
-              {...register('email', {
-                required: {
-                  value: true,
-                  message: 'Email requerido',
-                },
-                pattern: {
-                  value: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-                  message: 'Formato del email incorrecto',
-                },
-              })}
-            />
-            {errors.email && <span className={errors.email && 'span_error'}>{errors.email.message}</span>}
+              <p className='contact-right-label'>
+                <label>E-mail</label>
+              </p>
+              <input
+                className={errors.email && 'input_error'}
+                placeholder=''
+                type='text'
+                {...register('email', {
+                  required: {
+                    value: true,
+                    message: 'Email requerido',
+                  },
+                  pattern: {
+                    value: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                    message: 'Formato del email incorrecto',
+                  },
+                })}
+              />
+              {errors.email && <span className={errors.email && 'span_error'}>{errors.email.message}</span>}
 
-            <p className='contact-right-label'>
-              <label>Mensaje</label>
-            </p>
-            <textarea
-              className={errors.textarea && 'input_error'}
-              type='text'
-              {...register('textarea', {
-                required: { value: true, message: 'Mensaje requerido' },
-              })}
-            />
-            {errors.textarea && <span className={errors.textarea && 'span_error'}>{errors.textarea.message}</span>}
-            <div className='contact-buton-right'>
-              <input type='submit' className='btn-primary right' value='Enviar' />
-            </div>
-          </form>
-        </div>
+              <p className='contact-right-label'>
+                <label>Mensaje</label>
+              </p>
+              <textarea
+                className={errors.textarea && 'input_error'}
+                type='text'
+                {...register('textarea', {
+                  required: { value: true, message: 'Mensaje requerido' },
+                })}
+              />
+              {errors.textarea && <span className={errors.textarea && 'span_error'}>{errors.textarea.message}</span>}
+              <div className='contact-buton-right'>
+                <input type='submit' className='btn-primary right' value='Enviar' />
+              </div>
+            </form>
+          </div>
+        )}
       </div>
     </>
   );
