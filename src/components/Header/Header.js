cfd49a32be363dd016a15a3509a11620634a520c
Marcos Pozzo <pozzomarc@gmail.com>
2023-06-24 00:59:12 -0300

    typo fix in cambios reales header and on save automatic format

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,84 +1,92 @@
 /* eslint-disable eqeqeq */
-import React, { useEffect, useRef, useState } from 'react';
-import { PrimaryButton } from '..';
-import { ReactComponent as InstagramBrand } from '../../assets/images/header/instagram-brands.svg';
-import { ReactComponent as MailBrand } from '../../assets/images/header/mail.svg';
-import { ReactComponent as ShoppingBag } from '../../assets/images/header/shopping-bag.svg';
-import { ReactComponent as WhatsappBrand } from '../../assets/images/header/whatsapp-brands.svg';
+import React, { useEffect, useRef, useState } from 'react'
+import { PrimaryButton } from '..'
+import { ReactComponent as InstagramBrand } from '../../assets/images/header/instagram-brands.svg'
+import { ReactComponent as MailBrand } from '../../assets/images/header/mail.svg'
+import { ReactComponent as ShoppingBag } from '../../assets/images/header/shopping-bag.svg'
+import { ReactComponent as WhatsappBrand } from '../../assets/images/header/whatsapp-brands.svg'
 
-import navClose from '../../assets/images/header/nav-close.svg';
-import navOpen from '../../assets/images/header/nav-open.svg';
-import LogoWebp from '../../assets/images/header/paola_logo.webp';
+import navClose from '../../assets/images/header/nav-close.svg'
+import navOpen from '../../assets/images/header/nav-open.svg'
+import LogoWebp from '../../assets/images/header/paola_logo.webp'
 
-import { useSelector } from 'react-redux';
-import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux'
+import { Link, NavLink, useNavigate } from 'react-router-dom'
 
 export default function Header() {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
-  const { cartTotalQuantity } = useSelector((state) => state.cart);
+  const { cartTotalQuantity } = useSelector((state) => state.cart)
 
-  const [sidebar, setSidebar] = useState(false);
+  const [sidebar, setSidebar] = useState(false)
 
-  const showSidebar = () => setSidebar(!sidebar);
+  const showSidebar = () => setSidebar(!sidebar)
 
-  const closeSidebar = () => setSidebar(!sidebar);
+  const closeSidebar = () => setSidebar(!sidebar)
 
   const handleMoveToCart = () => {
     if (sidebar) {
-      closeSidebar();
+      closeSidebar()
     }
-    navigate('/carrito');
-  };
-  const [size, setSize] = useState(false);
+    navigate('/carrito')
+  }
+  const [size, setSize] = useState(false)
 
   useEffect(() => {
     const changeNavbarSize = () => {
       if (window.scrollY >= 60) {
-        setSize(true);
+        setSize(true)
       }
-    };
+    }
     // clean up code
-    window.removeEventListener('scroll', changeNavbarSize);
-    window.addEventListener('scroll', changeNavbarSize);
-    return () => window.removeEventListener('scroll', changeNavbarSize);
-  }, []);
+    window.removeEventListener('scroll', changeNavbarSize)
+    window.addEventListener('scroll', changeNavbarSize)
+    return () => window.removeEventListener('scroll', changeNavbarSize)
+  }, [])
 
   useEffect(() => {
     const changeNavbarSizeF = () => {
       if (window.scrollY < 59) {
-        setSize(false);
-      }
-    };
-    window.addEventListener('scroll', changeNavbarSizeF);
-    return () => window.removeEventListener('scroll', changeNavbarSizeF);
-  }, []);
-
-  const wrapperRef = useRef(null);
-  const wrapperRefIcon = useRef(null);
-
-  useEffect(() => {
-    document.addEventListener('click', handleClickOutside, false);
-    return () => {
-      document.removeEventListener('click', handleClickOutside, false);
-    };
-  }, [wrapperRef]);
-  const handleClickOutside = (event) => {
-    if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
-      if (wrapperRefIcon.current.className == 'navBar__icon__svg navBar__icon__svg--none fixed' && !wrapperRefIcon.current.contains(event.target)) {
-        //setIsVisible(false);
-        setSidebar(false);
-      } else {
-        setSidebar(true);
+        setSize(false)
       }
     }
-  };
+    window.addEventListener('scroll', changeNavbarSizeF)
+    return () => window.removeEventListener('scroll', changeNavbarSizeF)
+  }, [])
+
+  const wrapperRef = useRef(null)
+  const wrapperRefIcon = useRef(null)
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside, false)
+    return () => {
+      document.removeEventListener('click', handleClickOutside, false)
+    }
+  }, [wrapperRef])
+  const handleClickOutside = (event) => {
+    if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+      if (
+        wrapperRefIcon.current.className ==
+          'navBar__icon__svg navBar__icon__svg--none fixed' &&
+        !wrapperRefIcon.current.contains(event.target)
+      ) {
+        //setIsVisible(false);
+        setSidebar(false)
+      } else {
+        setSidebar(true)
+      }
+    }
+  }
 
   return (
     <>
       <div className={size ? 'navBar header--resize' : 'navBar'}>
-        <div ref={wrapperRefIcon} className='navBar__icon__svg navBar__icon__svg--none fixed' onClick={showSidebar}>
-          <img className='img-icono-close-open' src={navOpen} alt=''/>
+        <div
+          ref={wrapperRefIcon}
+          className='navBar__icon__svg navBar__icon__svg--none fixed'
+          onClick={showSidebar}
+        >
+          <img className='img-icono-close-open' src={navOpen} alt='' />
         </div>
 
         <div className='menu__logo'>
@@ -87,23 +95,44 @@ export default function Header() {
           </Link>
         </div>
 
-        <div ref={wrapperRef} className={sidebar ? 'navBar__menu navBar__menu--active' : 'navBar__menu'}>
+        <div
+          ref={wrapperRef}
+          className={
+            sidebar ? 'navBar__menu navBar__menu--active' : 'navBar__menu'
+          }
+        >
           <div className='menu__left'>
             <div className='pepe' onClick={closeSidebar}>
               <div className='container__icon__close'>
-                <img className='img-icono-close-open' src={navClose} alt=''/>
+                <img className='img-icono-close-open' src={navClose} alt='' />
               </div>
             </div>
-            <NavLink className='menu__item menu__link' to='/sobre-mi' onClick={closeSidebar}>
+            <NavLink
+              className='menu__item menu__link'
+              to='/sobre-mi'
+              onClick={closeSidebar}
+            >
               <p className='underlined'>Sobre Mi</p>
             </NavLink>
-            <NavLink className='menu__item menu__link ' to='/cambios-reales' onClick={closeSidebar}>
-              <p className=' underlined'>Cambios Realess</p>
+            <NavLink
+              className='menu__item menu__link '
+              to='/cambios-reales'
+              onClick={closeSidebar}
+            >
+              <p className=' underlined'>Cambios Reales</p>
             </NavLink>
-            <NavLink className='menu__item menu__link' to='/faq' onClick={closeSidebar}>
+            <NavLink
+              className='menu__item menu__link'
+              to='/faq'
+              onClick={closeSidebar}
+            >
               <p className=' underlined'>FAQ</p>
             </NavLink>
-            <NavLink className='menu__item menu__link' to='/contacto' onClick={closeSidebar}>
+            <NavLink
+              className='menu__item menu__link'
+              to='/contacto'
+              onClick={closeSidebar}
+            >
               <p className=' underlined '>Contacto</p>
             </NavLink>
           </div>
@@ -114,10 +143,20 @@ export default function Header() {
             </div>
             <div className='menu__item menu__link left-redes'>
               <div className='menu__redes '>
-                <a className='menu__redes__link' href='https://www.instagram.com/nutricion.paonicola/' target='_blank' rel='noopener noreferrer'>
+                <a
+                  className='menu__redes__link'
+                  href='https://www.instagram.com/nutricion.paonicola/'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
                   <InstagramBrand className='menu__redes__icono' />
                 </a>
-                <a className='menu__redes__link' href='mailto:nutricionista.nicola@gmail.com' target='_blank' rel='noopener noreferrer'>
+                <a
+                  className='menu__redes__link'
+                  href='mailto:nutricionista.nicola@gmail.com'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
                   <MailBrand className='menu__redes__icono' />
                 </a>
                 <a
@@ -139,5 +178,5 @@ export default function Header() {
         </div>
       </div>
     </>
-  );
+  )
 }
